570d429f9fe8ff34484b11452b23ba9349ebd591
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-06-07 00:30:55 +0530

    update

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
